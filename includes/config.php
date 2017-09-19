d06d9e7c5a18cd6eb56283d2a3d8f7a23ed9edde
darkshifty <rowanfuchs@hotmail.com>
2017-09-19 10:31:07 +0200

    removed old code

--- includes/config.php
+++ includes/config.php
@@ -20,16 +20,3 @@ $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
 header('Content-Type: text/html; charset=utf-8');
-
-// IP Proxy
-if ($HTTP_X_FORWARDED_FOR) {
-    echo "Proxy Name $HTTP_VIA";
-    echo "Proxy IP $REMOTE_ADDR";
-    echo gethostbyaddr();
-    echo "Your Real IP $HTTP_X_FORWARDED_FOR";
-} else {
-    // if access direct to Internet, without Proxy
-    echo "$REMOTE_ADDR";
-}
-	
-?>
